c0fc6d9548c380c6bc13ddc7306fb8174b69b9ab
zhaojianhui129 <zhaojianhui129@163.com>
2016-06-25 12:32:00 +0800

    测试

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
-phpinfo();
+//phpinfo();
+//Establish connection to AMQP
+$connection = new AMQPConnection();
+$connection->setHost('192.168.1.104');
+$connection->setLogin('qianxun');
+$connection->setPassword('qianxun');
+$connection->connect();
+
+//Create and declare channel
+$channel = new AMQPChannel($connection);
+
+//AMQPC Exchange is the publishing mechanism
+$exchange = new AMQPExchange($channel);
+try {
+    $routing_key = 'hello';
+    $queue = new AMQPQueue($channel);
+    $queue->setName($routing_key);
+    $queue->setFlags(AMQP_NOPARAM);
+    $queue->declareQueue();
+    
+    
+    $message = 'nowtime:'.time().'['.date('Y-m-d H:i:s').']';
+    $exchange->publish($message, $routing_key);
+    
+    $connection->disconnect();
+}catch (Exception $ex){
+    print_r($ex);
+}
 ?>
